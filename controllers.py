e030c017ff14e4a5403060953a6792aeff0ff68c
nkornien <nkornien@ucsc.edu>
2022-05-27 18:30:23 -0700

    Revert "modified auth.html"
    This reverts commit 297a94e1d77687e14fe93927393aeaf82d7bf8aa.

--- controllers.py
+++ controllers.py
@@ -1,26 +1,18 @@
 """
 This file defines actions, i.e. functions the URLs are mapped into
 The @action(path) decorator exposed the function at URL:
-
     http://127.0.0.1:8000/{app_name}/{path}
-
 If app_name == '_default' then simply
-
     http://127.0.0.1:8000/{path}
-
 If path == 'index' it can be omitted:
-
     http://127.0.0.1:8000/
-
 The path follows the bottlepy syntax.
-
 @action.uses('generic.html')  indicates that the action uses the generic.html template
 @action.uses(session)         indicates that the action uses the session
 @action.uses(db)              indicates that the action uses the db
 @action.uses(T)               indicates that the action uses the i18n & pluralization
 @action.uses(auth.user)       indicates that the action requires a logged in user
 @action.uses(auth)            indicates that the action requires the auth object
-
 session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
@@ -31,13 +23,13 @@ from .common import db, session, T, cache, auth, logger, authenticated, unauthen
 from py4web.utils.url_signer import URLSigner
 from .models import get_user_email
 from py4web.utils.form import Form, FormStyleBulma, FormStyleDefault, RadioWidget
-from py4web.utils.auth import Auth
+from pydal.validators import *
 
 url_signer = URLSigner(session)
 
 
 @action('index')
-@action.uses('index.html', db, url_signer, auth.user)
+@action.uses('display.html', db, auth.user, url_signer)
 def index():
     rows = db(db.budgets.user_id == get_user_email()).select()
     return dict(
@@ -47,12 +39,40 @@ def index():
     )
 
 
+@action('graph/<budget_id:int>')
+@action.uses('graph_display.html', db, auth.user, url_signer)
+def index(budget_id=None):
+    assert budget_id is not None
+
+    budget_name = db(db.budgets.id == budget_id).select()[0].name
+    rows = db(db.budgets.user_id == get_user_email()).select()
+    return dict(
+        my_callback_url=URL('my_callback', signer=url_signer),
+        rows=rows,
+        url_signer=url_signer,
+        budget_name=budget_name,
+        budget_id=budget_id,
+        get_budgets_url=URL('get_budgets', signer=url_signer),
+    )
+    
+    
+
+@action('get_budgets')
+@action.uses(url_signer.verify(), db)
+def get_images():
+    """Returns the list of images."""
+    budgets=db(db.budgets).select().as_list()
+    return dict(budgets=budgets)
+
+@action('login')
+@action.uses('login.html', auth, url_signer)
+def login():
+    return dict()
 
 
 @action('display')
 @action.uses('display.html', db, auth.user, url_signer)
 def display():
-
     return dict(
         my_callback_url=URL('my_callback', signer=url_signer),
         rows=db(db.budgets.user_id == get_user_email()).select(),
@@ -77,19 +97,20 @@ def create():
 
 
 @action('edit_budget/<budget_id:int>', method=["GET", "POST"])
-# need to fix .verify()
-@action.uses('edit_budget.html', db, auth.user, url_signer)
+@action.uses('edit_budget.html', db, auth.user, url_signer, url_signer.verify())
 def edit_budget(budget_id=None):
     assert budget_id is not None
 
     budget_name = db(db.budgets.id == budget_id).select()[0].name
     rows = db(db.budget_items.budget_id == budget_id).select()
+
     return dict(
         my_callback_url=URL('my_callback', signer=url_signer),
         rows=rows,
         budget_name=budget_name,
         budget_id=budget_id,
-        url_signer=url_signer
+        url_signer=url_signer,
+        # total=total
     )
 
 
@@ -102,22 +123,26 @@ def delete_budget(budget_id=None):
 
 
 @action('add_budget_item/<budget_id:int>', method=["GET", "POST"])
-@action.uses('add_budget_item.html', db, auth.user, url_signer.verify())
+@action.uses('add_budget_item.html', db, auth.user, url_signer, url_signer.verify())
 def add_budget_item(budget_id=None):
     assert budget_id is not None
     # FormStyleDefault.widgets['type']=RadioWidget() # type should be a radio button rather than text boz
-    form = Form([Field('name'), Field('amount'), Field('type')], csrf_session=session,
+    form = Form([Field('name'), Field('amount'), Field('type', requires=IS_IN_SET(['Expense', 'Income']))], csrf_session=session,
                 formstyle=FormStyleBulma)
     if form.accepted:
         db.budget_items.insert(
             budget_id=budget_id,
             name=form.vars['name'],
             amount=form.vars['amount'],
-            type=form.vars['type'])
-        redirect(URL(f'edit_budget/{budget_id}'))
+            type=form.vars['type']
+        )
 
+        update_budget(budget_id)
+
+        redirect(URL(f'edit_budget/{budget_id}', signer=url_signer))
     return dict(
         my_callback_url=URL('my_callback', signer=url_signer),
+        budget_id=budget_id,
         form=form,
         url_signer=url_signer
     )
@@ -134,11 +159,36 @@ def edit_budget_item(budget_items_id=None, budget_id=None):
         # Nothing found to be edited!
         redirect(URL('edit_budget', budget_id, signer=url_signer))
     # Edit form: it has record=
-    form = Form(db.budget_items, record=p, deletable=False, csrf_session=session, formstyle=FormStyleBulma)
+    form = Form(db.budget_items, record=p, deletable=False,
+                csrf_session=session, formstyle=FormStyleBulma)
     if form.accepted:
         # The update already happened!
+        update_budget(budget_id)
         redirect(URL('edit_budget', budget_id, signer=url_signer))
-    return dict(form=form, url_signer = url_signer, budget_id=budget_id)
+    return dict(
+        form=form,
+        url_signer=url_signer,
+        budget_id=budget_id,
+        my_callback_url=URL('my_callback', signer=url_signer)
+    )
+
+
+def update_budget(budget_id):
+    budget_items = db(db.budget_items.budget_id ==
+                      budget_id).select().as_list()
+
+    expenses = 0
+    income = 0
+    for budget_item in budget_items:
+        if budget_item['type'] == 'Expense':
+            expenses += budget_item['amount']
+        else:
+            income += budget_item['amount']
+
+    net_flow = income - expenses
+
+    db(db.budgets.id == budget_id).update(
+        expenses=expenses, income=income, net_flow=net_flow)
 
 
 @action('delete_budget_item/<budget_id:int>/<budget_item_id:int>')
@@ -147,4 +197,4 @@ def delete_budget(budget_id=None, budget_item_id=None):
     assert budget_id is not None
     assert budget_item_id is not None
     db(db.budget_items.id == budget_item_id).delete()
-    redirect(URL(f'edit_budget/{budget_id}'))
+    redirect(URL(f'edit_budget/{budget_id}', signer=url_signer))
